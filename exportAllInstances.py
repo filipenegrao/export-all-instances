2fd2f12e0278839fc311af37651aa5f80a9e0810
rdazvd <rdazvd@users.noreply.github.com>
2016-03-11 13:57:41 -0300

    another attempt to fix loop

--- exportAllInstances.py
+++ exportAllInstances.py
@@ -1,3 +1,10 @@
+#MenuTitle: Export All Instances
+# -*- coding: utf-8 -*-
+
+__doc__ = """
+Exports all instances of all opened files
+"""
+
 for font in Glyphs.fonts:
 	for instance in font.instances:
 		instance.generate('~/Desktop')
