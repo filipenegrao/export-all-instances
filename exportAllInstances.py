b1c578cb7f1daf3d7a58e05c3befe1c8a82b1f15
rdazvd <rdazvd@users.noreply.github.com>
2016-03-11 14:13:27 -0300

    path variable defined; .otf parameter added to the generate method

--- exportAllInstances.py
+++ exportAllInstances.py
@@ -5,6 +5,10 @@ __doc__ = """
 Exports all instances of all opened files
 """
 
+exportFolder = '~/Desktop'
+
 for font in Glyphs.fonts:
 	for instance in font.instances:
-		instance.generate('~/Desktop')
+		instance.generate('OTF', exportFolder)
+
+	Glyphs.showNotification('Export fonts', 'The export of %s was successful.' % (Glyphs.font.familyName))
